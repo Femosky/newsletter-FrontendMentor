5784b470f868b15885e98973da14df1d0f9a7f17
Femosky <31240841+Femosky@users.noreply.github.com>
2023-06-13 12:23:28 +0000

    javascript stuck

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+const emailInput = document.querySelector(".email-input");
+const submitInput = document.querySelector(".button");
+
+function emailChecker() {
+  const receivedInput = emailInput.value;
+  let found = false;
+
+  for (let i = 0; i < receivedInput.length; i++) {
+    if (receivedInput.charAt(i) === "@") {
+      found = true;
+      break;
+    }
+  }
+
+  return found;
+}
+
+submitInput.addEventListener("click", (event) => {
+  event.preventDefault(); // Prevents the default form submission behavior
+
+  if (emailChecker()) {
+    window.location.href = "success.html";
+  } else {
+    window.location.href = "error.html";
+  }
+});
+
 
